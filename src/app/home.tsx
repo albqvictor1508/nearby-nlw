7dcea69095a1097983593dc39f96d74cf36c9b66
albqvictor1508 <albq.victor@gmail.com>
2024-12-13 15:52:23 -0300

    incoming: creating categories component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, Alert } from "react-native";
+
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+	const [categories, setCategories] = useState();
+
+	useEffect(() => {
+		async function fetchCategorys() {
+			try {
+				const { data } = await api.get("/categories");
+				setCategories(data);
+			} catch (e) {
+				console.log(e);
+				Alert.alert("Categorias", "Não foi possível carregar as categorias");
+			}
+		}
+
+		fetchCategorys();
+	}, []);
+
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
 			<Text>Home</Text>
